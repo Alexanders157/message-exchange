8fb3ce30b5aa8a425dd1366f64f648ca3a384233
Alexander157 <alexander.157@yandex.ru>
2024-04-28 12:49:16 +0300

    Add Frontend design (HTML + CSS)

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -7,12 +7,58 @@
     <meta name="author" content="Пользователь">
     <meta name="robots" content="index, follow">
     <title>Создание сообщения</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f9f9f9;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        .message-form {
+            width: 80%;
+            max-width: 600px;
+            padding: 100px;
+            background-color: #ffffff;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+        .message-input {
+            width: 100%;
+            height: 150px;
+            padding: 10px;
+            font-size: 16px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            resize: none;
+        }
+        .message-button {
+            display: block;
+            width: 50%;
+            margin: 0 auto;
+            padding: 10px;
+            font-size: 16px;
+            background-color: #007bff;
+            color: #ffffff;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+    </style>
 </head>
 <body>
-<form method="POST" action="{{ route('store') }}">
-    @csrf
-    <textarea name="content"></textarea>
-    <button type="submit">Отправить</button>
-</form>
+
+<div class="message-form">
+    <h1> Отправить сообщение:</h1>
+    <form method="POST" action="{{ route('store') }}">
+        @csrf
+        <textarea class="message-input" name="content" placeholder="Введите сообщение"></textarea>
+        <br>
+        <br><button class="message-button" type="submit">Отправить</button>
+    </form>
+</div>
 </body>
 </html>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,13 +3,57 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Отображение сообщения">
+    <meta name="description" content="Сообщение">
     <meta name="author" content="Пользователь">
     <meta name="robots" content="index, follow">
     <title>Отображение сообщения</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f9f9f9;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        .message-container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 100px;
+            background-color: #ffffff;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            height: 300px;
+        }
+        .message-content {
+            font-size: 16px;
+            line-height: 1.6;
+            border: 1px solid #ccc;
+            padding: 10px;
+            border-radius: 5px;
+            word-wrap: break-word;
+            overflow-wrap: break-word;
+            overflow-y: auto;
+            max-height: 200px;
+            margin-top: -20px; /
+        }
+        .message-link {
+            color: #007bff;
+            margin-top: -10px;
+        }
+    </style>
 </head>
 <body>
-<p>{{ $message->content }}</p>
-<p>Ссылка на это сообщение: {{ url($message->slug) }}</p>
+
+<div class="message-container">
+    <h1 style="margin-top: -20px;"> Сообщение: </h1>
+    <br>
+    <p class="message-content">{{ $message->content }}</p>
+    <br>
+    <p class="message-link">Ссылка на это сообщение: <br>
+        <a href="{{ url($message->slug) }}">{{ url($message->slug) }}</a></p>
+</div>
 </body>
 </html>
